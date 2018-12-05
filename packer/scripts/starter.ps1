0f2f9743f8247eb66e5608caa233fa30724473e8
HDMP <30300775+HDMP@users.noreply.github.com>
2018-12-05 16:31:35 +0100

    Add files via upload

--- packer/scripts/starter.ps1
+++ packer/scripts/starter.ps1
@@ -8,20 +8,17 @@ Optimize-Volume -DriveLetter C #Laufwerk optimieren
 
 #-----------------------------------------------------Install Software-----------------------------------------------------#
 
-#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-#choco install firefox -y
-
-#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-#Install-Module PSWindowsUpdate -Force
-
-#Get-WUInstall -MicrosoftUpdate -AcceptAll
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install firefox -y
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module PSWindowsUpdate -Force
+Get-WUInstall -MicrosoftUpdate -AcceptAll
 
 #-------------------------------------------------------RDP & WINRM-------------------------------------------------------#
 
 #Enable Remote Desktop
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
-Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 1
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Set-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP -Enabled True
 
@@ -31,7 +28,6 @@ netsh advfirewall firewall set rule group="remote administration" new enable=yes
 netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
 Enable-WSManCredSSP -Force -Role Server
 
-Read-Host
-#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-#winrm set winrm/config/service/auth '@{Basic="true"}'
-#winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/client/auth '@{Basic="true"}'
